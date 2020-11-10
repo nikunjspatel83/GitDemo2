f498ca4b6d01a81e0c155d94153fe6a4539a50a4
Nikunj <nikunjspatel83@yahoo.com>
2020-11-09 18:01:03 -0600

    First commit

---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,27 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void Browserautomation()
+	{
+		System.out.println("Browserautomation");
+	}
+	
+	@Test
+	public void elementUI()
+	{
+		System.out.println("elementUI");
+	}
+	
+	@Test
+	public void Elementlogin()
+	{
+		System.out.println("Elementlogin");
+	}
+}
+
+
